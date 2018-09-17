5000122541a62bf964d4e0b2d133535d9e6ad850
yuluyao <wushenggoo@gmail.com>
2018-09-17 17:49:44 +0800

    save project

---
+++ app/src/main/java/com/yuluyao/exstatusbar/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.yuluyao.exstatusbar
+
+import android.graphics.Color
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    window.decorView.systemUiVisibility =
+        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+//        View.SYSTEM_UI_FLAG_FULLSCREEN or
+//        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+//        View.SYSTEM_UI_FLAG_IMMERSIVE or
+        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    if (Build.VERSION.SDK_INT >= 21) {
+      window.statusBarColor = 0x8800ffff.toInt()
+//      window.statusBarColor =Color.TRANSPARENT
+      window.navigationBarColor = 0x88ff00ff.toInt()
+    }
+
+    window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+    window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+
+
+  }
+}
